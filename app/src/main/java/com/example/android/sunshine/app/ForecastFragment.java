60b0431f6bf88b1249379d6a1cb582626672655d
Daniel Wallace <daniel.wallace.nz@gmail.com>
2015-09-13 06:42:29 +1200

    use settings location for data

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -1,9 +1,11 @@
 package com.example.android.sunshine.app;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.text.format.Time;
 import android.util.Log;
@@ -68,7 +70,13 @@ public class ForecastFragment extends Fragment {
         if (id == R.id.action_refresh) {
 
             FetchWeatherTask fetchWeatherTask = new FetchWeatherTask();
-            String postcode = "6021,nz";
+
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String postcode = prefs.getString(getString(R.string.pref_location_key),
+                    getString(R.string.pref_location_default));
+            Log.d(LOG_TAG, "postcode = " + postcode);
+
+//            String postcode = "6021,nz";
             fetchWeatherTask.execute(postcode);
 
             Log.d(LOG_TAG, "finding weather for postcode " + postcode);
